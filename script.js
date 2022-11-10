ad7791245b928ef119b2e08bd09b84b0528334ef
shameelvk <shameel3311@gmail.com>
2022-11-10 13:20:40 +0530

    added edit option

--- script.js
+++ script.js
@@ -7,28 +7,59 @@ let lists=document.getElementById("list");
 function add(){
     let inputData=input.value;
     let li=document.createElement("li");
-    li.textContent=inputData;
+    let span=document.createElement("span");
+    li.appendChild(span);
+    span.innerText=inputData;
     li.classList.add("list");
     lists.appendChild(li);
     input.value="";
     input.focus();
+
+
+    // trsh
     let del=document.createElement("i");
     console.log(del);
     del.classList.add("fas","fa-trash");
     li.appendChild(del);
 
+    // edit button
+    let edit=document.createElement("i");
+    console.log(edit);
+    edit.classList.add("fas","fa-edit");
+    li.appendChild(edit);
+
 }
 
 function delet(event){
-    if(event.target.classList[0]=="fas"){
+    if(event.target.classList[1]=="fa-trash"){
         let iteam=event.target.parentElement;
-        console.log(iteam);
         iteam.remove();
     }
    
     
 }
+function edit(event){
+    if(event.target.classList[1]=="fa-edit"){
+        let iteam=event.target.parentElement;
+        let spane=iteam.querySelector("span");
+        input.value=spane.innerText;
+        input.focus();
+        function del()
+        { 
+            let iteam=event.target.parentElement;
+            iteam.remove();
+        }
+        del();
+       
+    }  
+}
 
+
+
+
+
+
+lists.addEventListener("click",edit);
 lists.addEventListener("click",delet);
 input.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
